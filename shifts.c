42e585478ce9807e079c0bbdbb72ff5c524fb710
daviddcho <amazingdcho@gmail.com>
2021-11-16 23:55:38 -0500

    make tests for all casesg

--- shifts.c
+++ shifts.c
@@ -3,12 +3,14 @@
 
 int main() {
   int A[size] = {0, 4, -3, 0, 4, -1, 2, 0, 1, 5};
-  int X = 100, S = 0, i;
+  int X = 2, S = 0, i;
   for (i = 0; i < size; i++) {
     if (A[i] < 0) 
-      S += X << (-A[i]); 
+      //S += X << (-A[i]); 
+      S += -A[i] << X;
     else 
-      S += X >> A[i];
+      //S += X >> A[i];
+      S += A[i] >> X;
   }
   printf("A:");
   for (i = 0; i < size; i++) 
--- test.c
+++ test.c
@@ -5,8 +5,12 @@ typedef long long int word_t;
 int main() {
   word_t x;
   word_t cval = 2;
-  word_t argB = 5;
+  word_t argB = 23;
   // shift argb by cval
-  x = (signed) argB >> cval;
+  //x = (signed) argB >> cval;
+  //x = (unsigned) argB >> cval;
+  //x = (signed) argB << cval;
+  x = (unsigned) argB << cval;
+
   printf("%lld\n", x);
 }
