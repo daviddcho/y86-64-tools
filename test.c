b87ee7701e94d4bf72f3ccbe3b09c75b553fde0f
daviddcho <amazingdcho@gmail.com>
2021-11-17 19:24:55 -0500

    basic tests in one

--- test.c
+++ test.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 
 typedef long long int word_t;
+#define PACK_CC(z,s,o) (((z)<<2)|((s)<<1)|((o)<<0))
 
 int main() {
   word_t x;
   word_t cval = 2;
-  word_t argB = 23;
+  word_t argB = 10;
   // shift argb by cval
-  //x = (signed) argB >> cval;
-  //x = (unsigned) argB >> cval;
-  //x = (signed) argB << cval;
-  x = (unsigned) argB << cval;
+  //printf("shift %lld by %lld\n", argB, cval);
+  printf("shift 0x%llx by 0x%llx\n", argB, cval);
+  printf("****************\n");
 
-  printf("%lld\n", x);
+  // logical tests
+  x = (signed) argB >> cval;
+  printf("logical right: 0x%llx\n", x);
+  x = (signed) argB << cval;
+  printf("logical left: 0x%llx\n", x);
+
+  // arithmetic tests
+  x = (unsigned) argB >> cval;
+  printf("arithmetic right: 0x%llx\n", x);
+  x = (unsigned) argB << cval;
+  printf("arithmetic left: 0x%llx\n", x);
+
+  //unsigned char b = PACK_CC(1,0,0);
+  //printf("%u\n", b);
 }
