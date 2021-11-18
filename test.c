cf98b1698261bdfc77d658c85b3a07a32d2b6122
daviddcho <amazingdcho@gmail.com>
2021-11-17 22:05:56 -0500

    playing around, this should be simple..

--- test.c
+++ test.c
@@ -1,34 +1,50 @@
 #include <stdio.h>
+#include <limits.h>
+#include <math.h>
 
 typedef long long int word_t;
 #define PACK_CC(z,s,o) (((z)<<2)|((s)<<1)|((o)<<0))
 
 int main() {
   word_t x;
-  word_t cval = 2;
-  word_t argB = 100;
+  word_t cval = 1;
+  //word_t argb = LLONG_MAX;
+  word_t argb = -1;
+  printf("argb 0x%llx, 0x%llx\n", (unsigned long long) argb, (signed long long) argb); 
+  printf("argb 0x%x, 0x%x\n", (unsigned) argb, (signed) argb); 
   // shift argb by cval
-  printf("shift 0x%llx by 0x%llx\n", argB, cval);
+  printf("shift 0x%llx by 0x%llx\n", argb, cval);
   printf("****************\n");
 
   // logical tests
-  x = (unsigned) argB >> cval;
+  x = (unsigned long long) argb >> cval;
+  //x = argb >> cval;
   printf("logical right: 0x%llx, %lld\n", x, x);
-  x = (unsigned) argB << cval;
+  x = (unsigned long long) argb << cval;
+  //x = argb << cval;
   printf("logical left: 0x%llx, %lld\n", x, x);
+  printf("%lu bytes\n", sizeof(x));
 
   // arithmetic tests
-  x = (signed) argB >> cval;
+  //x = (signed long long) argb >> cval;
+  x = argb >> cval;
   printf("arithmetic right: 0x%llx, %lld\n", x, x);
-  x = (signed) argB << cval;
+  printf("%lu\n", sizeof(x));
+  //x = (signed long long) argb << cval;
+  x = argb << cval;
   printf("arithmetic left: 0x%llx, %lld\n", x, x);
 
   //unsigned char b = PACK_CC(1,0,0);
   //printf("%u\n", b);
 }
 
-
 /* 
 https://stackoverflow.com/questions/44694957/the-difference-between-logical-shift-right-arithmetic-shift-right-and-rotate-r
 https://stackoverflow.com/questions/7622/are-the-shift-operators-arithmetic-or-logical-in-c/22734721
+
+arithmetic left shift has overflow problem bc 
+u can push out the signed bit and change the sign 
+logical shift is unsigned so no problem
+
+try cval=1, argb=LLONG_MAX
 */
