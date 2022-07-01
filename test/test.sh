68460eebcd9dddf9578c2843e4eac22a1dc3ef81
daviddcho <amazingdcho@gmail.com>
2022-07-01 17:12:19 -0400

    usage

--- test/test.sh
+++ test/test.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 (cd ../misc/ && make clean)
 (cd ../misc/ && make)
-echo "**** BEGIN TEST ****"
+printf "\n**** BEGIN SHIFTS TEST ****\n"
 ../misc/yas shifts.ys
 ../misc/yis shifts.yo
 gcc shifts.c && ./a.out
 
-#./misc/yas test.ys
-#./misc/yis test.yo
+#printf "\n**** BEGIN SINGLE OP TEST ****\n"
+#../misc/yas test_op.ys
+#../misc/yis test_op.yo
 #gcc test.c && ./a.out
-#./misc/yas test_ovf.ys
-#./misc/yis test_ovf.yo
+#printf "\n**** BEGIN OVF TEST ****\n"
+#../misc/yas test_ovf.ys
+#../misc/yis test_ovf.yo
